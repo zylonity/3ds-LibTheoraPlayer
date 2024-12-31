2becc3a9571521fedc9fe1b5d30356112fd51596
zylo_dev <70657892+zylonity@users.noreply.github.com>
2024-12-31 14:48:46 +0000

    Added some extra notes in the main file and stopped video looping

--- source/main.c
+++ source/main.c
@@ -15,6 +15,8 @@ int main(int argc, char* argv[]) {
 	int cursor = 0;
 	int from = 0;
 
+	bool startedPlaying = false;
+
 	romfsInit();
 	ndspInit();
 	gfxInitDefault();
@@ -24,15 +26,13 @@ int main(int argc, char* argv[]) {
 	consoleInit(GFX_BOTTOM, NULL);
 	hidSetRepeatParameters(25, 5);
 
-	//osSetSpeedupEnable(true);
+	//osSetSpeedupEnable(true); //Sets the new3ds faster mode or something, I wouldn't know, my 3ds is 14 years old.
 
 
 	// Create screens
 	top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
-	//printf("%s.\n", th_version_string());
-
-	//ndspSetCallback(TP_audioCallback, NULL);
+	ndspSetCallback(TP_audioCallback, NULL);
 
 	while(aptMainLoop()){
 		hidScanInput();
@@ -42,16 +42,29 @@ int main(int argc, char* argv[]) {
 		if (kDown & KEY_START)
 			break;
 
-		if (!THEORA_isplaying) {
+		//Starts playing the file, sets the file
+		if (!THEORA_isplaying && startedPlaying == false) {
 			TP_changeFile("sdmc:/videos/test.ogg");
-		} 
+		}
+		else{
+			startedPlaying = true;
+		}
 
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 			C2D_TargetClear(top, C2D_Color32(20, 29, 31, 255));
 			C2D_SceneBegin(top);
+
+
 			if (THEORA_isplaying && THEORA_HasVideo(&THEORA_vidCtx))
 				frameDrawAtCentered(&THEORA_frame, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0.5f, THEORA_scaleframe, THEORA_scaleframe);
+
+
 		C3D_FrameEnd(0);
+
+		if (!THEORA_isplaying && startedPlaying)
+		{
+			TP_exitThread(); //Stops playing the video at the end, otherwise it just loops through
+		}
 	}
 
 	TP_exitThread();
